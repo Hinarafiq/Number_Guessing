249ac4875ca2213589a81f2d8d8753200846961b
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2024-04-24 11:55:03 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {name:"userGuessNumber",
+type: "number",
+message: "Please guess a number"}
+]);
+ if(answer.userGuessNumber === randomNumber){
+    console.log("Congratulations! you guess right number");
+ } 
+ else {
+    console.log("You guessed wrong");
+ }
